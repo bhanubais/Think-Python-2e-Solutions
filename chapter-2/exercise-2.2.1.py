44aa0691c36279fdaa129011d5e03b5bc911520c
Bhanu Pratap Singh <bhanubais@gmail.com>
2017-06-15 02:19:22 +0530

    2.2.1 solution

---
+++ chapter-2/exercise-2.2.1.py
@@ -0,0 +1,15 @@
+"""
+exercise-2.2.1
+
+The volume of a sphere with radius r is (4/3)*πr3. 
+What is the volume of a sphere with radius 5?
+
+"""
+import math
+
+
+radius = 5
+volume = 4.0 / 3 * math.pi * radius**3
+
+print("volume:", round(volume, 4), "unit^3")
+
