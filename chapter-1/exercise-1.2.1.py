df18f484a40e083b28e758305dced26ebe6e7034
Bhanu Pratap Singh <bhanubais@gmail.com>
2017-06-15 01:47:34 +0530

    exercise-1.2.1 solved.

---
+++ chapter-1/exercise-1.2.1.py
@@ -0,0 +1,13 @@
+"""
+exercise-1.2.1
+How many seconds are there in 42 minutes 42 seconds?
+"""
+
+mins = 42
+secs = 42
+
+# 1 minutes = 60 seconds
+# x minutes = x * 60 seconds
+total_secs = mins * 60 + secs
+
+print("There are", total_secs, "seconds in 42 minutes 42 seconds.")
