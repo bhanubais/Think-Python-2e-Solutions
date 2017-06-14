595c8df014e0ffdcaecac685a0be1c2455838496
Bhanu Pratap Singh <bhanubais@gmail.com>
2017-06-15 02:07:52 +0530

    corrected answer

---
+++ chapter-1/exercise-1.2.2.py
@@ -0,0 +1,13 @@
+"""
+exercise-1.2.2
+
+How many miles are there in 10 kilometers? 
+Hint: there are 1.61 kilometers in a mile.
+
+"""
+
+# 1.61 kms = 1 miles
+# 1 km = 1 / 1.61 miles
+# x km = x / 1.61 miles
+
+print ("There are", round(10 / 1.61, 2), "miles in 10 kilometers.")
